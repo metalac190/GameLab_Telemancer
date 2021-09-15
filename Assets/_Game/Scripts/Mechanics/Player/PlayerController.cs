5ce953663a406b36632652cc92e87a80434773af
Bill Dang <sparkfire.ffs@gmail.com>
2021-09-15 16:31:21 -0500

    Installed and set up new input system

--- Assets/_Game/Scripts/Mechanics/Player/PlayerController.cs
+++ Assets/_Game/Scripts/Mechanics/Player/PlayerController.cs
@@ -1,17 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour {
-    
-    // Start is called before the first frame update
-    void Start() {
-        
+
+    private CharacterController controller;
+
+
+
+    private void Awake() {
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update() {
-        
+    private void FixedUpdate() {
+
+    }
+
+    // ------------------
+
+    public void Move(InputAction.CallbackContext value) {
+        Debug.Log(value.ReadValue<Vector2>());
     }
 
 }
