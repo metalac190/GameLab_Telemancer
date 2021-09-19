66b000cfa68d97401a966579ac535309a77d20e8
Cameron <cameron.meyer02@gmail.com>
2021-09-19 14:01:35 -0500

    Lightning In Warp Bolt Prefab

---
+++ Assets/LightningRotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningRotator : MonoBehaviour
+{
+    [SerializeField] float _rotateSpeed = 3f;
+
+    void Update()
+    {
+        transform.Rotate(0f, 0f, _rotateSpeed * Time.deltaTime, Space.Self);
+    }
+}
