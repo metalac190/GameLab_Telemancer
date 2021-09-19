4afddc31514284d5b608f1841f4a2687ecfd6a0c
Cameron <cameron.meyer02@gmail.com>
2021-09-19 17:40:33 -0500

    Warp Bolt Smoke + Tweaks

--- Assets/LightningRotator.cs
+++ Assets/LightningRotator.cs
@@ -6,8 +6,12 @@ public class LightningRotator : MonoBehaviour
 {
     [SerializeField] float _rotateSpeed = 3f;
 
+    [SerializeField] GameObject _lightningTarget;
+    [SerializeField] float _LTRotSpeed = -3f;
+
     void Update()
     {
         transform.Rotate(0f, 0f, _rotateSpeed * Time.deltaTime, Space.Self);
+        //_lightningTarget.transform.Rotate(_LTRotSpeed * 0.25f * Time.deltaTime, _LTRotSpeed * Time.deltaTime, 0f, Space.Self);
     }
 }
