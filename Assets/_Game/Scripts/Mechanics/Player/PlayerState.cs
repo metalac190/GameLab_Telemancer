6f349546d36f627fe5dd0cfea538a87f75d23c97
Brandon Coffey <bmc180001@utdallas.edu>
2021-09-20 12:56:48 -0500

    Setup for Unlocks (Warp, Residue) and Interactions

---
+++ Assets/_Game/Scripts/Mechanics/Player/PlayerState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Mechanics.Player
+{
+    public class PlayerState : MonoBehaviour
+    {
+        [SerializeField] private Vector3 _lastCheckpoint;
+        [SerializeField] private bool _unlockedWarp = false;
+        [SerializeField] private bool _unlockedResidue = false;
+
+        public event Action<bool, bool> OnChangeUnlocks = delegate { };
+
+        private void OnValidate()
+        {
+            UpdateUnlocks();
+        }
+
+        private void Start()
+        {
+            UpdateUnlocks();
+        }
+
+        private void UpdateUnlocks()
+        {
+            OnChangeUnlocks.Invoke(_unlockedWarp, _unlockedResidue);
+        }
+    }
+}
