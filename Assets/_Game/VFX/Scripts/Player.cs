643b24152f662c643563c03146c128caf65b0e63
Brandon Coffey <bmc180001@utdallas.edu>
2021-09-19 20:10:30 -0500

    Revert 'Trim Excess' to merge VFX

---
+++ Assets/_Game/VFX/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] GameObject _projectile;
+
+    private GameObject _projectileInstance;
+
+    private void Update()
+    {
+        Fire();
+    }
+
+    public void Fire()
+    {
+        if (Input.GetKeyDown("space") || Input.GetMouseButtonDown(0))
+        {
+            if(_projectileInstance != null)
+            {
+                Destroy(_projectileInstance);
+            }
+
+            _projectileInstance = Instantiate(_projectile, transform.position, Quaternion.identity);
+        }
+    }
+}
