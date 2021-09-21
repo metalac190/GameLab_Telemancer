5ad4684749356c2adf59058e3f34aae3e449c116
Noah Sims <noahsims24@gmail.com>
2021-09-21 14:58:09 -0500

    Prototype Warp Rod
    - need player.teleport before this can be tested

---
+++ Assets/_Game/Scripts/Mechanics/LevelMechanics/WarpRod.cs
@@ -0,0 +1,28 @@
+﻿using Mechanics.WarpBolt;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpRod : MonoBehaviour, IWarpInteractable
+{
+    [SerializeField] private GameObject _warpPad;
+    [SerializeField] private Vector3 _teleportOffset = Vector3.up;
+
+    public bool OnWarpBoltImpact(BoltData data)
+    {
+        data.PlayerController.Teleport(_warpPad.transform, _teleportOffset);
+
+        // Boolean return value to determine whether to dissipate warp bolt after impact
+        return true;
+    }
+
+    public bool OnSetWarpResidue(BoltData data)
+    {
+        return true;
+    }
+
+    public void OnActivateWarpResidue(BoltData data)
+    {
+        OnWarpBoltImpact(data);
+    }
+}
