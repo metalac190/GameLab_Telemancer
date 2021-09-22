db0ea689ffba8a8e6042c56c0e0a9ae74d553734
Brett <ginsutime78@gmail.com>
2021-09-22 01:36:05 -0500

    Addition of audio system files already done
    Contains the majority of the core functionality of the system.

---
+++ Assets/_Game/Scripts/Mechanics/AudioSystem/AudioObjClasses/SFXEvent.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace AudioSystem
+{
+    public abstract class SFXEvent : ScriptableObject
+    {
+        [Header("General Settings (Subject to Change)")]
+        [SerializeField] AudioClip[] possibleClips = new AudioClip[0];
+        [SerializeField] AudioMixerGroup mixer = null;
+        [Space(15)]
+
+        [Range(0, 128)]
+        [SerializeField]
+        int priority = 128;
+
+        [SerializeField]
+        [MinMaxRange(0, 1)]
+        RangedFloat volume = new RangedFloat(.8f, .8f);
+
+        [SerializeField]
+        [MinMaxRange(0, 2)]
+        RangedFloat pitch = new RangedFloat(.95f, 1.05f);
+
+        [SerializeField]
+        [Range(-1, 1)]
+        private float stereoPan = 0;
+
+        [SerializeField]
+        [Range(0, 1)]
+        float spatialBlend = 0;
+
+        [Header("3D Settings")]
+        [SerializeField] float attenuationMin = 1;
+        [SerializeField] float attenuationMax = 500;
+
+        int clipIndex = 0;
+
+        public AudioClip Clip => possibleClips[clipIndex];
+        public AudioMixerGroup Mixer => mixer;
+
+        public int Priority => priority;
+        public float Volume { get; private set; }
+        public float Pitch { get; private set; }
+        public float StereoPan => stereoPan;
+        public float SpatialBlend => spatialBlend;
+        public float AttenuationMin => attenuationMin;
+        public float AttenuationMax => attenuationMax;
+
+        protected void SetVariationValues()
+        {
+            clipIndex = Random.Range(0, possibleClips.Length);
+            Volume = Random.Range(volume.MinValue, volume.MaxValue);
+            Pitch = Random.Range(pitch.MinValue, pitch.MaxValue);
+        }
+    }
+}
