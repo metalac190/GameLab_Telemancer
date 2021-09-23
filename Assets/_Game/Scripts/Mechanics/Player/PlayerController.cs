e611155513cfd8d2a014b774106d21be72ec97d4
Bill Dang <sparkfire.ffs@gmail.com>
2021-09-23 00:33:26 -0500

    Update scene and add pause flag

--- Assets/_Game/Scripts/Mechanics/Player/PlayerController.cs
+++ Assets/_Game/Scripts/Mechanics/Player/PlayerController.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
+
+#pragma warning disable 0649 // Disable "Field is never assigned" warning for SerializeField
+
     private CharacterController controller;
 
+    [Header("Horizontal Movement")]
     public float moveSpeed;
 
     private Vector3 moveVelocity;
-    private Vector2 xzInput;
+    private Vector3 xzInput;
+
+    [Header("Vertical Movement")]
+    [SerializeField] private int jumpForce;
+
+    [Header("General Control")]
+    public UnityEvent OnPlayerDeath;
+    public bool flag_cantAct;
+
+#pragma warning restore 0649
 
     // -------------------------------------------------------------------------------------------
 
@@ -21,15 +34,18 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        moveVelocity = ((xzInput.x * transform.right) + (xzInput.y * transform.forward)) * moveSpeed;
-        controller.Move(moveVelocity * Time.deltaTime);
+        if(!flag_cantAct) {
+            moveVelocity = ((xzInput.x * transform.right) + (xzInput.z * transform.forward)) * moveSpeed;
+            controller.Move(moveVelocity * Time.deltaTime);
+        }
     }
 
     // -------------------------------------------------------------------------------------------
 
     public void Move(InputAction.CallbackContext value)
     {
-        xzInput = value.ReadValue<Vector2>();
+        Vector2 inputValue = value.ReadValue<Vector2>();
+        xzInput = new Vector3(inputValue.x, 0f, inputValue.y);
         //Debug.Log(value.ReadValue<Vector2>());
     }
 
