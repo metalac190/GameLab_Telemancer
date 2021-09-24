aa989831f8d6f29e5e33218512323ad2da3b10f7
Bill Dang <sparkfire.ffs@gmail.com>
2021-09-24 14:44:37 -0500

    Added new teleport function and added debug buttons

---
+++ Assets/_Game/Scripts/Mechanics/Player/Editor/PlayerEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlayerController))]
+public class PlayerEditor : Editor {
+
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        PlayerController pc = target as PlayerController;
+
+        if(GUILayout.Button("Teleport With Transform")) {
+            pc.DebugTeleportWithTransform();
+        }
+        if(GUILayout.Button("Teleport To Transform")) {
+            pc.DebugTeleportToTransform();
+        }
+        if(GUILayout.Button("Teleport To Vector3")) {
+            pc.DebugTeleportToVector3();
+        }
+    }
+
+}
