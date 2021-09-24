cba2100d7d2ad994bc6a4aaedce19a12580f2f83
Bill Dang <sparkfire.ffs@gmail.com>
2021-09-24 01:47:46 -0500

    Added basic teleport, jumping, and gravity

--- Assets/_Game/Scripts/Mechanics/Player/PlayerController.cs
+++ Assets/_Game/Scripts/Mechanics/Player/PlayerController.cs
@@ -11,13 +11,16 @@ public class PlayerController : MonoBehaviour {
     private CharacterController controller;
 
     [Header("Horizontal Movement")]
-    public float moveSpeed;
+    [Range(0,50)] public float moveSpeed;
 
     private Vector3 moveVelocity;
     private Vector3 xzInput;
 
     [Header("Vertical Movement")]
-    [SerializeField] private int jumpForce;
+    [SerializeField] [Range(0,20)] private float jumpForce;
+    [SerializeField] [Range(0,50)] private float risingGravity, fallingGravity;
+    [SerializeField] [Range(0,1)] private float floatTime;
+    private bool flag_jump, flag_canFloat;
 
     [Header("General Control")]
     public UnityEvent OnPlayerDeath;
@@ -27,31 +30,56 @@ public class PlayerController : MonoBehaviour {
 
     // -------------------------------------------------------------------------------------------
 
-    private void Awake()
-    {
+    private void Awake() {
         controller = GetComponent<CharacterController>();
     }
 
-    private void FixedUpdate()
-    {
+    private void FixedUpdate() {
+        // Movement
         if(!flag_cantAct) {
-            moveVelocity = ((xzInput.x * transform.right) + (xzInput.z * transform.forward)) * moveSpeed;
-            controller.Move(moveVelocity * Time.deltaTime);
+            // XZ Axis
+            moveVelocity = (((xzInput.x * transform.right) + (xzInput.z * transform.forward)) * moveSpeed) + (moveVelocity.y * transform.up);
+
+            // Y Axis
+            if(flag_jump) { // Jump
+                moveVelocity.y = jumpForce;
+                flag_jump = false;
+            } else { // Gravity
+                // TODO - float
+                moveVelocity.y -= (moveVelocity.y > 0 ? risingGravity : fallingGravity) * Time.fixedDeltaTime;
+            }
+
+            // Apply
+            controller.Move(moveVelocity * Time.fixedDeltaTime);
         }
     }
 
     // -------------------------------------------------------------------------------------------
 
-    public void Move(InputAction.CallbackContext value)
-    {
+    public void Move(InputAction.CallbackContext value) {
         Vector2 inputValue = value.ReadValue<Vector2>();
         xzInput = new Vector3(inputValue.x, 0f, inputValue.y);
         //Debug.Log(value.ReadValue<Vector2>());
     }
 
-    public void Teleport(Transform other, Vector3 offset = default)
-    {
+    public void Jump(InputAction.CallbackContext value) {
+        if(value.performed) {
+            //Teleport(GameObject.Find("Cube").transform);
+
+            if(/*grounded*/true)
+                flag_jump = true;
+        }
+    }
+
+    public void Teleport(Transform other, Vector3 offset = default) {
         // TODO: Swap teleport player and other transform
+        Vector3 oldPlayerPos = transform.position;
+
+        controller.enabled = false;
+        transform.position = other.position + offset;
+        other.position = oldPlayerPos;
+        controller.enabled = true;
+
         Debug.Log("Teleport to " + other.gameObject.name + " at " + other.position + offset, other.gameObject);
     }
 }
