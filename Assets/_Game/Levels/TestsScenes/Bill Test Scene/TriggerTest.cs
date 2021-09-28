ec477e671e88230f953ee99124f73a065e4fe1f3
Bill Dang <sparkfire.ffs@gmail.com>
2021-09-28 17:39:01 -0500

    Fix (/workaround for) OnTriggerExit not working when teleporting

---
+++ Assets/_Game/Levels/TestsScenes/Bill Test Scene/TriggerTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTest : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("enter");
+    }
+
+    private void OnTriggerExit(Collider other) {
+        Debug.Log("exit");
+    }
+
+}
