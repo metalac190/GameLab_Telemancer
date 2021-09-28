98fe85c6c875261a695769b0e80760c25037595d
Noah Sims <noahsims24@gmail.com>
2021-09-28 16:20:33 -0500

    Somewhat fixed bug
    So the problem is that on teleport, the player object is disable, moved, then re-enabled. Disabling an object doesn't trigger OnTriggerExit, because unity. This will come down to Bill to figure out how to handle, as it could cause problems for other people's OnTriggerExits as well.
    At the very least, I did fix a problem with the pressure plates. Now, if two things are on the same pressure plate, it won't activate it multiple times

--- Assets/_Game/Scripts/Mechanics/LevelMechanics/PressurePlate.cs
+++ Assets/_Game/Scripts/Mechanics/LevelMechanics/PressurePlate.cs
@@ -6,8 +6,7 @@ public class PressurePlate : MonoBehaviour
 {
     [SerializeField] private List<LevelActivatable> _activatables = new List<LevelActivatable>(); // the list of objects to be toggled by this pressure plate
     private int _id = 0;
-    [SerializeField] private bool _isPressed = false;
-    //private bool _
+    private bool _isPressed = false; // if two things are on the pressure plate at once, don't want to double activate it
 
     private void Awake()
     {
@@ -26,36 +25,52 @@ public class PressurePlate : MonoBehaviour
     {
         if (!_isPressed)
         {
-            if (other.gameObject.layer != LayerMask.NameToLayer("Warp Bolt"))
+            if (other.gameObject.layer != LayerMask.NameToLayer("Warp Bolt") && other.gameObject.layer != LayerMask.NameToLayer("Ground Detector"))
             {
                 _isPressed = true;
                 foreach (LevelActivatable obj in _activatables)
                 {
                     if (obj != null) { obj.Toggle(_id); }
                 }
-                Debug.Log("pressure plate entered");
+                
             }
         }
+        Debug.Log("pressure plate entered: " + other.gameObject.name);
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.layer != LayerMask.NameToLayer("Warp Bolt"))
+        if (other.gameObject.layer != LayerMask.NameToLayer("Warp Bolt") && other.gameObject.layer != LayerMask.NameToLayer("Ground Detector"))
         {
-            _isPressed = true;
+            if (!_isPressed)
+            {
+                _isPressed = true;
+                foreach (LevelActivatable obj in _activatables)
+                {
+                    if (obj != null) { obj.Toggle(_id); }
+                }
+            }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.layer != LayerMask.NameToLayer("Warp Bolt"))
+        if (other.gameObject.layer != LayerMask.NameToLayer("Warp Bolt") && other.gameObject.layer != LayerMask.NameToLayer("Ground Detector"))
         {
-            _isPressed = false;
-            StartCoroutine(DeactivateOnCooldown());
-            Debug.Log("pressure plate exited: " + other.gameObject.name);
+            if (_isPressed)
+            {
+                _isPressed = false;
+                foreach (LevelActivatable obj in _activatables)
+                {
+                    if (obj != null) { obj.Toggle(_id); }
+                }
+            }
+            //StartCoroutine(DeactivateOnCooldown());
         }
+        Debug.Log("pressure plate exited: " + other.gameObject.name);
     }
 
+    /*
     IEnumerator DeactivateOnCooldown()
     {
         yield return new WaitForSeconds(0.1f);
@@ -67,6 +82,7 @@ public class PressurePlate : MonoBehaviour
             }
         }
     }
+    */
 
     private void OnDrawGizmos()
     {
