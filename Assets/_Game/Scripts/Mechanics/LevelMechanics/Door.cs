a06cadc8d81d3867eeba6bfcbe0cb77fd50394b7
Noah Sims <noahsims24@gmail.com>
2021-10-01 14:33:56 -0500

    Temporal Rift Prototype
    And half of a Door script

---
+++ Assets/_Game/Scripts/Mechanics/LevelMechanics/Door.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : LevelActivatable
+{
+    [Header("Door")]
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private Vector3 _openPosition = new Vector3();
+    [SerializeField] private Vector3 _closedPosition = new Vector3();
+
+    protected override void OnActivate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void OnDeactivate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
