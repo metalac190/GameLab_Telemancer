06bb3b9dc78351ee6626f57d47c671d532e00da0
Noah Sims <noahsims24@gmail.com>
2021-10-09 13:55:21 -0500

    Started on resetting puzzle objects on death

--- Assets/_Game/Scripts/Mechanics/LevelMechanics/Base/Temp_LargeRock.cs
+++ Assets/_Game/Scripts/Mechanics/LevelMechanics/Base/Temp_LargeRock.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 /// Summary:
 /// Temporary Large / Big Rock Script. Mainly for reference and testing
 /// Inherits from Warp Residue Interactable, and overrides the WarpBoltImpact() to swap itself with the player
-public class Temp_LargeRock : WarpResidueInteractable
+public class Temp_LargeRock : WarpResidueInteractable, IResettable
 {
     // A simple offset for the teleportation location
     [SerializeField] private Vector3 _teleportOffset = Vector3.up;
+    // the position of the object on start
+    private Transform _spawnPoint = null; 
+
+    private void Start()
+    {
+        _spawnPoint = transform;
+    }
 
     /// Called on either WarpBoltImpact or WarpResidueActivated, see WarpResidueInteractable
     public override bool OnWarpBoltImpact(BoltData data)
@@ -20,4 +27,10 @@ public class Temp_LargeRock : WarpResidueInteractable
     }
 
     // The other 3 functions can be inherited from for extra capabilities
+
+    public void OnSceneReset()
+    {
+        transform.position = _spawnPoint.position;
+        transform.rotation = _spawnPoint.rotation;
+    }
 }
