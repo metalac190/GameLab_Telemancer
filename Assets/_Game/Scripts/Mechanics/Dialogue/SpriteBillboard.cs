4fa3d4a4ea42f95991e7c85a909a9fc566fb4d37
Xander <xanderyoussef@gmail.com>
2021-10-11 17:42:25 -0500

    Added dialogue interact popup that always faces player

---
+++ Assets/_Game/Scripts/Mechanics/Dialogue/SpriteBillboard.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteBillboard : MonoBehaviour
+{
+    void LateUpdate() 
+    {
+        transform.LookAt(Camera.main.transform.position, new Vector3(0, 1, 0));
+    }
+}
