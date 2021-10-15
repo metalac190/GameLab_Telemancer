94f0bf2249f6ce9f4d4e6bd9dc2865e47485b3b6
Brandon Coffey <bmc180001@utdallas.edu>
2021-10-15 16:26:19 -0500

    Implement other Animations

--- Assets/_Game/Scripts/Mechanics/Player/PlayerAnimator.cs
+++ Assets/_Game/Scripts/Mechanics/Player/PlayerAnimator.cs
@@ -7,14 +7,25 @@ namespace Mechanics.Player
     // Mostly used by the Player Casting Script and the 'Player Interactions' Script
     public class PlayerAnimator : MonoBehaviour
     {
+        [Header("Animator")]
         [SerializeField] private Animator _animator;
-        [SerializeField] private string _castBoltTrigger = "pointTrigger";
-        [SerializeField] private string _warpTrigger = "snapTrigger";
-        [SerializeField] private string _useResidueTrigger = "interactTrigger";
-        [SerializeField] private string _boltDissipateTrigger = "dissipateTrigger";
+        [Header("Idle Parameters")]
+        [SerializeField] private string _petToadTrigger = "pet_toad";
+        [SerializeField] private string _startledTrigger = "startled";
+        [SerializeField] private string _idleTwitchTrigger = "idle_twitch";
+        [Header("Jumping / Landing Parameters")]
+        [SerializeField] private string _jumpTrigger = "jump";
+        [SerializeField] private string _airTimeFloat = "air_time";
+        [SerializeField] private string _landTrigger = "land";
+        [Header("Casting Parameters")]
+        [SerializeField] private string _residueUnlockedBool = "residue_unlocked";
+        [SerializeField] private string _castBoltTrigger = "point_cast";
+        [SerializeField] private string _warpTrigger = "snap_teleport";
+        [SerializeField] private string _interactableTrigger = "interact_hitInteractable";
+        [SerializeField] private string _boltDissipateTrigger = "dissipate";
 
         private bool _missingAnimator;
-        private float _fallTime;
+        private float _jumpTime;
 
         private void Awake()
         {
@@ -31,95 +42,119 @@ namespace Mechanics.Player
             ResetToIdle();
         }
 
+        #region Basic Actions
+
         public void OnPetToad()
         {
+            if (_missingAnimator) return;
+            _animator.SetTrigger(_petToadTrigger);
         }
 
         public void OnStartle()
         {
+            if (_missingAnimator) return;
+            _animator.SetTrigger(_startledTrigger);
         }
 
         public void OnJump()
         {
-            _fallTime = Time.time;
-        }
-
-        public void OnFall()
-        {
+            if (_missingAnimator) return;
+            ResetActionTriggers();
+            _animator.SetTrigger(_jumpTrigger);
+            _jumpTime = Time.time;
         }
 
         public void OnLand()
         {
-            float time = Time.time - _fallTime;
-            if (time < 1) {
-                OnSimpleLand();
-            } else if (time < 2) {
-                OnAnimatedLand();
-            } else {
-                OnDeathLand();
-            }
-        }
-
-        private void OnSimpleLand()
-        {
-        }
-
-        private void OnAnimatedLand()
-        {
-        }
-
-        private void OnDeathLand()
-        {
+            if (_missingAnimator) return;
+            ResetActionTriggers();
+            _animator.SetFloat(_airTimeFloat, Time.time - _jumpTime);
+            _animator.SetTrigger(_landTrigger);
         }
 
         public void OnKill()
         {
         }
 
+        #endregion
+
+        #region Casting Actions
+
+        public void SetResidueUnlocked(bool unlocked)
+        {
+            if (_missingAnimator) return;
+            _animator.SetBool(_residueUnlockedBool, unlocked);
+        }
+
         public void OnCastBolt()
         {
             if (_missingAnimator) return;
+            ResetCastingTriggers();
             _animator.SetTrigger(_castBoltTrigger);
-            _animator.ResetTrigger(_warpTrigger);
-            _animator.ResetTrigger(_useResidueTrigger);
-            _animator.ResetTrigger(_boltDissipateTrigger);
         }
 
-        public void OnUseResidue()
-        {
-            if (_missingAnimator) return;
-            _animator.SetTrigger(_useResidueTrigger);
-        }
-
+        // Player warped (right click) with warp unlocked. Play anim and return to idle.
         public void OnInstantWarp()
         {
             if (_missingAnimator) return;
+            ResetActionTriggers();
             _animator.SetTrigger(_warpTrigger);
         }
 
+        // Player hit interactable with bolt. Play anim and return to idle.
         public void OnInteractableWarp()
         {
             if (_missingAnimator) return;
-            _animator.SetTrigger(_useResidueTrigger);
+            ResetActionTriggers();
+            _animator.SetTrigger(_interactableTrigger);
         }
 
-        public void OnNoAction()
+        // Player used residue. Play hit interactable and return to idle.
+        // TODO: Should this be a different animation?
+        public void OnUseResidue()
+        {
+            OnInteractableWarp();
+        }
+
+        // Player was holding magic, but the bolt dissipated. Return to Idle
+        public void OnBoltDissipate()
         {
             if (_missingAnimator) return;
             _animator.SetTrigger(_boltDissipateTrigger);
         }
 
+        #endregion
+
+        #region Helper Functions
+
+        // Resets all triggers and returns animation to idle
+        // For instance, after warping or after landing a fall
         public void ResetToIdle()
         {
             if (_missingAnimator) return;
-            // Resets all triggers and returns animation to idle
-            // For instance, after warping or after landing a fall
-            _animator.ResetTrigger(_castBoltTrigger);
-            _animator.ResetTrigger(_warpTrigger);
-            _animator.ResetTrigger(_useResidueTrigger);
-            _animator.ResetTrigger(_boltDissipateTrigger);
+            ResetActionTriggers();
+            ResetCastingTriggers();
             _animator.Rebind();
             _animator.Update(0);
         }
+
+        private void ResetActionTriggers()
+        {
+            _animator.ResetTrigger(_petToadTrigger);
+            _animator.ResetTrigger(_startledTrigger);
+            _animator.ResetTrigger(_idleTwitchTrigger);
+            _animator.ResetTrigger(_jumpTrigger);
+            _animator.ResetTrigger(_landTrigger);
+        }
+
+        private void ResetCastingTriggers()
+        {
+            _animator.ResetTrigger(_castBoltTrigger);
+            _animator.ResetTrigger(_warpTrigger);
+            _animator.ResetTrigger(_interactableTrigger);
+            _animator.ResetTrigger(_boltDissipateTrigger);
+        }
+
+        #endregion
     }
 }
