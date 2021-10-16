9df10fdb2a10436be4a7e1fa3bd97c6b3374fcb1
Brett <ginsutime78@gmail.com>
2021-10-16 14:34:57 -0500

    Initial changes, adding volume to SOs, setting up prefabs in scenes

---
+++ Assets/_Game/Levels/00_MainMenu/Scripts/MenuMusicTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AudioSystem;
+
+public class MenuMusicTrigger : MonoBehaviour
+{
+    [SerializeField] MusicEvent mainMenuMusic = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (mainMenuMusic != null)
+        {
+            mainMenuMusic.Play();
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/Mechanics/AudioSystem/LevelMusicTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LevelMusicTrigger : MonoBehaviour
+{
+    public UnityEvent OnTriggerMusic;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            OnTriggerMusic?.Invoke();
+            GetComponent<Collider>().enabled = false;
+        }
+    }
+}
