816c09fb1772dec88d4e8db1008c4dd83b93e404
Brett <ginsutime78@gmail.com>
2021-10-16 16:22:11 -0500

    More setup

--- Assets/_Game/Scripts/Mechanics/AudioSystem/LevelMusicTrigger.cs
+++ Assets/_Game/Scripts/Mechanics/AudioSystem/LevelMusicTrigger.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LevelMusicTrigger : MonoBehaviour
 {
     public UnityEvent OnTriggerMusic;
+    public UnityEvent OnStopMusic;
+    [SerializeField] Button deathButton;
+    [SerializeField] Button restartButton;
+
+    private void Start()
+    {
+        deathButton.onClick.AddListener(PlayerDeath);
+        restartButton.onClick.AddListener(LevelRestart);
+    }
 
     void OnTriggerEnter(Collider col)
     {
@@ -14,5 +25,15 @@ public class LevelMusicTrigger : MonoBehaviour
             OnTriggerMusic?.Invoke();
             GetComponent<Collider>().enabled = false;
         }
+    }
+
+    void PlayerDeath()
+    {
+        OnStopMusic?.Invoke();
+    }
+
+    void LevelRestart()
+    {
+        OnStopMusic?.Invoke();
     }
 }
