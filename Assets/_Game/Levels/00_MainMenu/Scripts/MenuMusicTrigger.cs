a7dfcb49cc8685da05bd47247a77c879a5c0885c
Brett <ginsutime78@gmail.com>
2021-10-16 16:44:40 -0500

    Additional music setup

--- Assets/_Game/Levels/00_MainMenu/Scripts/MenuMusicTrigger.cs
+++ Assets/_Game/Levels/00_MainMenu/Scripts/MenuMusicTrigger.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using AudioSystem;
+using UnityEngine.UI;
 
 public class MenuMusicTrigger : MonoBehaviour
 {
     [SerializeField] MusicEvent mainMenuMusic = null;
+    [SerializeField] Button continueButton;
+    [SerializeField] Button newGameButton;
 
-    // Start is called before the first frame update
     void Start()
     {
+        continueButton.onClick.AddListener(OnContinue);
+        newGameButton.onClick.AddListener(OnNewGame);
+
         if (mainMenuMusic != null)
         {
             mainMenuMusic.Play();
         }
+    }
+
+    // Add code after build index updated to check which level so 
+    // that you can potentially use a crossfade through Play() instead of Stop()
+    void OnContinue()
+    {
+        mainMenuMusic.Stop();
+    }
+
+    // Consider using play once build index updated as well
+    void OnNewGame()
+    {
+        mainMenuMusic.Stop();
     }
 }
--- Assets/_Game/Scripts/Mechanics/AudioSystem/LevelMusicTrigger.cs
+++ Assets/_Game/Scripts/Mechanics/AudioSystem/LevelMusicTrigger.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class LevelMusicTrigger : MonoBehaviour
 {
