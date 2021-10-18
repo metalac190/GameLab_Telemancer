597fa7a2b59fa5ba195e6086b3102993d749fcbe
Henry <htwerner01@gmail.com>
2021-10-18 14:10:19 -0500

    Fix various UI bugs
    - pause menu options closes with esc
    - removed ability to click buttons through options menu
    - fixed objects not resetting when clicking restart level
    - fixed issue involving options menu default values
    - made wording more clear for locked spells

--- Assets/_Game/Scripts/UI/OptionsMenu/OptionsMenu.cs
+++ Assets/_Game/Scripts/UI/OptionsMenu/OptionsMenu.cs
@@ -28,6 +28,7 @@ public class OptionsMenu : MonoBehaviour
     {
         // Add listener
         UIEvents.current.OnOpenOptionsMenu += OnMenuOpen;
+        UIEvents.current.OnPauseGame += delegate(bool b) { if (!b) _optionsMenu.SetActive(false); };
         
         // Ensure menu is hidden
         _optionsMenu.SetActive(false);
@@ -35,6 +36,13 @@ public class OptionsMenu : MonoBehaviour
 
     private void OnMenuOpen()
     {
+        // Set prefs to default if they don't exist
+        foreach (var pref in _prefs)
+        {
+            if (!PlayerPrefs.HasKey(pref.Key))
+                PlayerPrefs.SetFloat(pref.Key, pref.Value);
+        }
+        
         UIEvents.current.ReloadSettings();
         _optionsMenu.SetActive(true);
     }
@@ -54,7 +62,7 @@ public class OptionsMenu : MonoBehaviour
         OnMenuClose();
     }
 
-    public void RestoreDefualtSettings()
+    public void RestoreDefaultSettings()
     {
         // Set all player prefs to default values
         foreach (var pref in _prefs)
