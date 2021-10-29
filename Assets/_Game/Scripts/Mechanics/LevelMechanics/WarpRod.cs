c67df9c986e2ee55e49b8cb662a62424fe0ea0eb
Noah Sims <noahsims24@gmail.com>
2021-10-28 19:12:03 -0500

    Fixed issues with Relay Stone and Door Prefabs caused by the new models
    Fixes:
    - Improved colliders on doors
    - Opening a door only moves the door, and not the whole frame
    - Fixed issues in level 3 level prefab with doors  being out of place, caused by changes to the prefab.
    - Relay stones fire bolts from the hole in the stone, rather than the center of the object
    - Warp Rod Wisps now shoot to a crystal in the warp pad, not the base of the warp pad

--- Assets/_Game/Scripts/Mechanics/LevelMechanics/WarpRod.cs
+++ Assets/_Game/Scripts/Mechanics/LevelMechanics/WarpRod.cs
@@ -10,14 +10,19 @@ public class WarpRod : WarpResidueInteractable
     [SerializeField] private Vector3 _teleportOffset = Vector3.up;
 
     [Header("Wisp")]
+    [SerializeField] private bool _shootWisps = true;
     [SerializeField] GameObject _wispPrefab = null;
+    private GameObject _wispReciever = null;
+
     [Header("Emission")]
+    [SerializeField] private bool _useEmission = true;
     [SerializeField] private GameObject _crystal = null;
     [SerializeField] private float _baseIntensity = 100f;
     [SerializeField] private float _intensityMultiplier = 5f;
     [SerializeField] private float _emisionFrequency = 2f;
     private Material _crystalMat = null;
     private Color _crystalColor;
+
     private bool _isWispOnCooldown = false;
 
     private void Start()
@@ -27,21 +32,28 @@ public class WarpRod : WarpResidueInteractable
             _crystalMat = _crystal.GetComponent<Renderer>().material;
             _crystalColor = _crystalMat.GetColor("_EmissiveColor");
         }
+        if(_warpPad != null)
+        {
+            _wispReciever = _warpPad.GetComponent<WarpPad>().WispReciever;
+        }
     }
 
     private void FixedUpdate()
     {
         float intensity = _intensityMultiplier * ((Mathf.Sin(Time.time * _emisionFrequency) + 1) / 2) + 1; // sin wave cycles through values between 0 and 1
 
-        _crystalMat?.SetColor("_EmissiveColor", _crystalColor * intensity * _baseIntensity);
+        if (_useEmission)
+        {
+            _crystalMat?.SetColor("_EmissiveColor", _crystalColor * intensity * _baseIntensity);
+        }
 
-        if (intensity > (_intensityMultiplier + 0.999f) && !_isWispOnCooldown && _wispPrefab != null) // shoot wisp at peak of sin wave
+        if (intensity > (_intensityMultiplier + 0.99f) && !_isWispOnCooldown && _shootWisps && _wispPrefab != null) // shoot wisp at peak of sin wave
         {
             StartCoroutine(WispCooldown());
-            Vector3 heading = (_warpPad.transform.position - transform.position).normalized;
+            Vector3 heading = (_wispReciever.transform.position - transform.position).normalized;
             Quaternion face = Quaternion.LookRotation(heading);
             GameObject wisp = Instantiate(_wispPrefab, _crystal.transform.position, face);
-            wisp.GetComponent<ConnectionWisp>()._Target = _warpPad.transform.position;
+            wisp.GetComponent<ConnectionWisp>()._Target = _wispReciever.transform.position;
         }
 
     }
@@ -61,7 +73,7 @@ public class WarpRod : WarpResidueInteractable
     IEnumerator WispCooldown()
     {
         _isWispOnCooldown = true;
-        yield return new WaitForSeconds(0.25f);
+        yield return new WaitForSeconds(0.15f);
         _isWispOnCooldown = false;
     }
 
@@ -69,6 +81,6 @@ public class WarpRod : WarpResidueInteractable
     {
         Gizmos.color = Color.blue;
         if (_warpPad != null)
-            Gizmos.DrawLine(transform.position, _warpPad.transform.position);
+            Gizmos.DrawLine(transform.position, _warpPad.GetComponent<WarpPad>().WispReciever.transform.position);
     }
 }
