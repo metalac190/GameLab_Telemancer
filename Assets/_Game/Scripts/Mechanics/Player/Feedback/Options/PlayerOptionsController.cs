8784f860fb015e7b3b5488f568df779ca16b682c
Katie S <sauter.katie@gmail.com>
2021-11-23 14:24:47 -0600

    Merge branch 'develop' into art/level-2-Enviro

--- Assets/_Game/Scripts/Mechanics/Player/Feedback/Options/PlayerOptionsController.cs
+++ Assets/_Game/Scripts/Mechanics/Player/Feedback/Options/PlayerOptionsController.cs
@@ -6,33 +6,62 @@ namespace Mechanics.Player.Feedback.Options
 {
     public class PlayerOptionsController : MonoBehaviour
     {
+        public static PlayerOptionsController Instance;
+
         [SerializeField] private PlayerLevelSelector _levelSelector = null;
         [SerializeField] private PlayerOptionSelector _invincibility = null;
         [SerializeField] private PlayerOptionSelector _infiniteJumps = null;
+        [SerializeField] private PlayerOptionSelectorAdvanced _boltUnlocked = null;
+        [SerializeField] private PlayerOptionSelectorAdvanced _warpUnlocked = null;
+        [SerializeField] private PlayerOptionSelectorAdvanced _residueUnlocked = null;
         [SerializeField] private PlayerOptionSelector _noBoltCooldown = null;
         [SerializeField] private PlayerOptionSelector _noWarpCooldown = null;
         [SerializeField] private PlayerOptionSelector _noResidueCooldown = null;
         [SerializeField] private PlayerOptionSelector _infiniteBoltDistance = null;
         [SerializeField] private PlayerOptionsSlider _boltMoveSpeed = null;
+        [SerializeField] private PlayerOptionSelector _mortalTed = null;
 
         public PlayerLevelSelector LevelSelector => _levelSelector;
         public PlayerOptionSelector Invincibility => _invincibility;
         public PlayerOptionSelector InfiniteJumps => _infiniteJumps;
+        public PlayerOptionSelectorAdvanced BoltUnlocked => _boltUnlocked;
+        public PlayerOptionSelectorAdvanced WarpUnlocked => _warpUnlocked;
+        public PlayerOptionSelectorAdvanced ResidueUnlocked => _residueUnlocked;
         public PlayerOptionSelector NoBoltCooldown => _noBoltCooldown;
         public PlayerOptionSelector NoWarpCooldown => _noWarpCooldown;
         public PlayerOptionSelector NoResidueCooldown => _noResidueCooldown;
         public PlayerOptionSelector InfiniteBoltDistance => _infiniteBoltDistance;
         public PlayerOptionsSlider BoltMoveSpeed => _boltMoveSpeed;
+        public PlayerOptionSelector MortalTed => _mortalTed;
 
-        public void SetData(PlayerOptionsData data)
+        public event Action OnDisable = delegate { };
+
+        private void Start()
         {
-            _invincibility.SetCurrentItem(data.Invincibility);
-            _infiniteJumps.SetCurrentItem(data.InfiniteJumps);
-            _noBoltCooldown.SetCurrentItem(data.NoBoltCooldown);
-            _noWarpCooldown.SetCurrentItem(data.NoWarpCooldown);
-            _noResidueCooldown.SetCurrentItem(data.NoResidueCooldown);
-            _infiniteBoltDistance.SetCurrentItem(data.InfiniteBoltDistance);
-            _boltMoveSpeed.SetCurrentValue(data.BoltMoveSpeed);
+            if (Instance != null) {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+        }
+
+        public void Refresh()
+        {
+            _invincibility.Refresh();
+            _infiniteJumps.Refresh();
+            _boltUnlocked.Refresh();
+            _warpUnlocked.Refresh();
+            _residueUnlocked.Refresh();
+            _noBoltCooldown.Refresh();
+            _noWarpCooldown.Refresh();
+            _noResidueCooldown.Refresh();
+            _infiniteBoltDistance.Refresh();
+            _boltMoveSpeed.Refresh();
+        }
+
+        public void Disable()
+        {
+            OnDisable?.Invoke();
         }
     }
 }
