ee9e2a50f5e4db31edb62bdd30080e110e8acdf3
Henry <htwerner01@gmail.com>
2021-12-05 22:47:34 -0600

    Apply credits fixes, Darken scroll pickup UI, Add fade-from-black
    Also fixes a bug with the options menu that was introduced after gold build

--- Assets/_Game/Levels/Credits/CreditsSequence.cs
+++ Assets/_Game/Levels/Credits/CreditsSequence.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.PlayerLoop;
@@ -11,6 +12,7 @@ public class CreditsSequence : MonoBehaviour
 {
     [SerializeField] private GameObject _titleCard, _designCard, _artCard, _soundCard, _progCard;
     [SerializeField] private Image _fadeImage;
+    [SerializeField] private TMP_Text _speedrunResults;
     [SerializeField] private float _initialPause = 3;
     [SerializeField] private float _cardDuration = 10;
     [SerializeField] private float _fadeDuration = 0.3f;
@@ -20,6 +22,9 @@ public class CreditsSequence : MonoBehaviour
         _titleCard.SetActive(true);
         _fadeImage.gameObject.SetActive(true);
         _fadeImage.color = new Color(0.8396f, 0.8396f, 0.8396f, 0f);
+        _speedrunResults.enabled = PlayerPrefs.GetFloat("SpeedrunTimer", 0) == 1;
+        GetTimerResults();
+        
         StartCoroutine(Cutscene());
     }
 
@@ -27,6 +32,41 @@ public class CreditsSequence : MonoBehaviour
     {
         if (Keyboard.current.escapeKey.wasPressedThisFrame)
             ExitToMainMenu();
+    }
+
+    private void GetTimerResults()
+    {
+        float lvl1 = PlayerPrefs.GetFloat("Level1Time", 0);
+        float lvl2 = PlayerPrefs.GetFloat("Level2Time", 0);
+        float lvl3 = PlayerPrefs.GetFloat("Level3Time", 0);
+        float total = lvl1 + lvl2 + lvl3;
+        string results = "";
+
+        float min = Mathf.FloorToInt(lvl1 / 60);
+        float sec = Mathf.FloorToInt(lvl1 % 60);
+        float ms = (lvl1 % 1) * 1000;
+        results += "Level 1 Time: ";
+        results += lvl1 == 0 ? "--:--.---" : $"{min:00}:{sec:00}.{ms:000}";
+        
+        min = Mathf.FloorToInt(lvl2 / 60);
+        sec = Mathf.FloorToInt(lvl2 % 60);
+        ms = (lvl2 % 1) * 1000;
+        results += "  |  Level 2 Time: ";
+        results += lvl2 == 0 ? "--:--.---" : $"{min:00}:{sec:00}.{ms:000}";
+        
+        min = Mathf.FloorToInt(lvl3 / 60);
+        sec = Mathf.FloorToInt(lvl3 % 60);
+        ms = (lvl3 % 1) * 1000;
+        results += "  |  Level 3 Time: ";
+        results += lvl3 == 0 ? "--:--.---" : $"{min:00}:{sec:00}.{ms:000}";
+        
+        min = Mathf.FloorToInt(total / 60);
+        sec = Mathf.FloorToInt(total % 60);
+        ms = (total % 1) * 1000;
+        results += "  |  Final Time: ";
+        results += (lvl1 == 0 || lvl2 == 0 || lvl3 == 0) ? "incomplete" : $"{min:00}m {sec:00}s {ms:000}ms";
+
+        _speedrunResults.text = results;
     }
 
     private IEnumerator Cutscene()
